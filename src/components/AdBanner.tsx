9381ef0999ad69424d72e286869f75e883ad59e1
masdelmon72 <101636797+masdelmon72@users.noreply.github.com>
2025-11-01 19:29:22 +0100

    Update AdBanner.tsx

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -11,11 +11,12 @@ const AdBanner = () => {
 
   return (
     <div style={{ textAlign: "center", margin: "2rem 0" }}>
+      {/* quando Google ti approva, sostituirai "1234567890" con il tuo vero slot */}
       <ins
         className="adsbygoogle"
         style={{ display: "block" }}
         data-ad-client="ca-pub-5007111395758189"
-        data-ad-slot="1234567890"  {/* <-- quando Google ti approva, sostituirai con il tuo vero slot */}
+        data-ad-slot="1234567890"
         data-ad-format="auto"
         data-full-width-responsive="true"
       ></ins>
